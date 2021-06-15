a17f623c63a9820ab587478400af45a323d36e1a
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-06-15 09:38:44 +0800

    feat: proxy

---
+++ proxy.js
@@ -0,0 +1,23 @@
+const obj = new Proxy({}, {
+  get: function (target, propKey, receiver) {
+    console.log(`getting ${propKey}!`);
+    return Reflect.get(target, propKey, receiver);
+  },
+  set: function (target, propKey, value, receiver) {
+    console.log(`setting ${propKey}!`);
+    return Reflect.set(target, propKey, value, receiver);
+  }
+});
+
+obj.count = 1;
+obj.count;
+
+var proxy = new Proxy({}, {
+  get: function(target, propKey) {
+    return 35;
+  }
+});
+
+proxy.time // 35
+proxy.name // 35
+proxy.title // 35
